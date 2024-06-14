91b8ec6e9aa2a29ce2a1fb8bd8edba0d02d4b92e
altbdoor <lancersupraskyline@gmail.com>
2024-06-14 23:34:10 +0800

    add dropdown to change models

--- force-gpt3.user.js
+++ force-gpt3.user.js
@@ -2,8 +2,9 @@
 // @name        Force GPT3
 // @namespace   altbdoor
 // @match       https://chatgpt.com/*
-// @grant       none
-// @version     1.2
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @version     1.3
 // @author      altbdoor
 // @run-at      document-start
 // @updateURL   https://github.com/altbdoor/userscripts/raw/master/force-gpt3.user.js
@@ -14,12 +15,19 @@
 const originalFetch = unsafeWindow.fetch;
 
 unsafeWindow.fetch = async (url, config) => {
-    if (url.includes('/backend-api/conversation') && config.method === 'POST') {
+    const gptModel = await GM.getValue('gptModel', '');
+
+    if (
+        gptModel !== '' &&
+        gptModel !== 'auto' &&
+        url.includes('/backend-api/conversation') &&
+        config.method === 'POST'
+    ) {
         try {
             const body = JSON.parse(config.body);
             config.body = JSON.stringify({
                 ...body,
-                model: 'text-davinci-002-render-sha',
+                model: gptModel,
             });
         } catch (error) {
             console.error('Error parsing JSON body:', error);
@@ -29,3 +37,41 @@ unsafeWindow.fetch = async (url, config) => {
     const response = await originalFetch(url, config);
     return response;
 };
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // add style
+    const style = document.createElement('style');
+    style.innerHTML = `
+        .toggleContainer {
+            position: absolute; right: 12rem; top: 0.5rem;
+        }
+        .toggleContainer select {
+            border-radius: 9999px;
+        }
+    `;
+    document.head.append(style);
+
+    // add dropdown
+    const toggleContainer = document.createElement('div');
+    toggleContainer.classList.add('toggleContainer');
+
+    toggleContainer.innerHTML = `
+        <select>
+            <option value="auto">Auto</option>
+            <option value="text-davinci-002-render-sha">GPT 3.5</option>
+        </select>
+    `;
+    document.body.appendChild(toggleContainer);
+
+    const select = toggleContainer.querySelector('select');
+    select.onchange = (evt) => {
+        GM.setValue('gptModel', evt.target.value);
+        console.log(`[force-gpt3] changing model to ${evt.target.value}`);
+    };
+
+    const selectVal = await GM.getValue(
+        'gptModel',
+        'text-davinci-002-render-sha',
+    );
+    select.value = selectVal;
+});
