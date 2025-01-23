b3194cc361155ec599e67ae434014f6bd630b281
Ng Han Seng <lancersupraskyline@gmail.com>
2025-01-23 17:05:56 +0800

    they started using URL objects

--- force-gpt3.user.js
+++ force-gpt3.user.js
@@ -4,7 +4,7 @@
 // @match       https://chatgpt.com/*
 // @grant       GM.setValue
 // @grant       GM.getValue
-// @version     1.8
+// @version     1.9
 // @author      altbdoor
 // @run-at      document-start
 // @updateURL   https://github.com/altbdoor/userscripts/raw/master/force-gpt3.user.js
@@ -26,10 +26,11 @@ windowRef.fetch = async (url, config) => {
         "gptModel",
         "text-davinci-002-render-sha",
     );
+    const fixedUrl = typeof url === 'string' ? url : url.toString();
 
     if (
         gptModel !== "auto" &&
-        url.includes("/backend-api/conversation") &&
+        fixedUrl.includes("/backend-api/conversation") &&
         config.method === "POST"
     ) {
         try {
