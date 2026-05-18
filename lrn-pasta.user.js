09f561a96eb96ad4b743e9df2ba3944de47fa6c8
altbdoor <lancersupraskyline@gmail.com>
2026-05-18 18:57:19 +0800

    behind those pallets, brother!

--- lrn-pasta.user.js
+++ lrn-pasta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LRN pasta
 // @namespace    altbdoor
-// @version      0.2
+// @version      0.3
 // @description  Take hold of a weapon and shield, and rise to help me.
 // @author       altbdoor
 // @match        https://*.course.lrn.com/*
@@ -11,8 +11,103 @@
 // @downloadURL  https://github.com/altbdoor/userscripts/raw/master/lrn-pasta.user.js
 // ==/UserScript==
 
+const handlePasta = () => {
+  const title =
+    document.title ||
+    window.parent?.document?.title ||
+    window.top?.document?.title ||
+    "";
+
+  const description = [
+    ...document.querySelectorAll(".ip-description app-text-content"),
+  ]
+    .map((el) => el.textContent.trim())
+    .filter((txt) => !!txt)
+    .join(", ");
+
+  const question =
+    document.querySelector(
+      [
+        ".ip-question-title",
+        ".ip-content-text",
+        "#PROFILER_ASSESSMENT_CONTAINER #PAGE_CONTENT",
+      ].join(", "),
+    )?.textContent ?? "";
+
+  if (!question) {
+    alert("unable to find question");
+    return;
+  }
+
+  const answers = [
+    ...(document.querySelector(".decision-radio-opt")?.children ?? []),
+  ].map(
+    (el, idx) => `${String.fromCharCode(65 + idx)}. ${el.textContent.trim()}`,
+  );
+  if (answers.length === 0) {
+    alert("unable to find answers");
+    return;
+  }
+
+  GM.setClipboard(
+    [
+      title ? `Title: ${title.trim()}` : "",
+      description ? `Description: ${description}` : "",
+      `Question: ${question.trim()}`,
+      "",
+      ...answers,
+    ]
+      .join("\n")
+      .trim(),
+    "text",
+  );
+};
+
+const handleNext = () => {
+  /** @type {HTMLButtonElement | null} */
+  const nextBtn = document.querySelector("#NAV_NEXT");
+  if (
+    nextBtn &&
+    !nextBtn.disabled &&
+    nextBtn.getAttribute("aria-disabled") !== "true"
+  ) {
+    nextBtn.click();
+    return;
+  }
+
+  const accordion = document.querySelectorAll(
+    ".js-track-spaceKey[role='button']:not(.viewed)",
+  );
+  if (accordion.length > 0) {
+    accordion.forEach((btn, index) => {
+      setTimeout(() => {
+        /** @type {HTMLButtonElement} */ (btn).click();
+      }, index * 200);
+    });
+    return;
+  }
+
+  /** @type {HTMLButtonElement | null} */
+  const vidPlayBtn = document.querySelector("#VIDEO_PLAY_BUTTON");
+  if (
+    vidPlayBtn &&
+    !vidPlayBtn.disabled &&
+    vidPlayBtn.getAttribute("aria-disabled") !== "true"
+  ) {
+    vidPlayBtn.click();
+
+    /** @type {HTMLVideoElement | null} */
+    const vidElem = document.querySelector("#COURSE_VIDEO");
+    if (vidElem) {
+      vidElem.playbackRate = 2;
+    }
+  }
+};
+
 (function () {
   "use strict";
+
+  document.documentElement.classList.add("ip-pageAnimations-off");
 
   const css = `
     html, body, * {
@@ -25,55 +120,16 @@
   style.textContent = css;
   (document.head || document.documentElement).appendChild(style);
 
+  /** @type {{ [key: string]: Function }} */
+  const eventMaps = {
+    F3: handlePasta,
+    ArrowRight: handleNext,
+  };
+
   window.addEventListener("keydown", (evt) => {
-    if (evt.key !== "F3") {
-      return;
+    if (eventMaps[evt.key]) {
+      evt.preventDefault();
+      eventMaps[evt.key]();
     }
-
-    evt.preventDefault();
-
-    const title =
-      document.title ||
-      window.parent?.document?.title ||
-      window.top?.document?.title ||
-      "";
-
-    const description = [
-      ...document.querySelectorAll(".ip-description app-text-content"),
-    ]
-      .map((el) => el.textContent.trim())
-      .filter((txt) => !!txt)
-      .join(", ");
-
-    const question =
-      document.querySelector(".ip-question-title, .ip-content-text")
-        ?.textContent ?? "";
-    if (!question) {
-      alert("unable to find question");
-      return;
-    }
-
-    const answers = [
-      ...(document.querySelector(".decision-radio-opt")?.children ?? []),
-    ].map(
-      (el, idx) => `${String.fromCharCode(65 + idx)}. ${el.textContent.trim()}`,
-    );
-    if (answers.length === 0) {
-      alert("unable to find answers");
-      return;
-    }
-
-    GM.setClipboard(
-      [
-        title ? `Title: ${title.trim()}` : "",
-        description ? `Description: ${description}` : "",
-        `Question: ${question.trim()}`,
-        "",
-        ...answers,
-      ]
-        .join("\n")
-        .trim(),
-      "text",
-    );
   });
 })();
